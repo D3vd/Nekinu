480a2b7ced60e137e203c28d218cdf0c50889773
R3l3ntl3ss <dev.91096@gmail.com>
2019-03-29 22:05:56 +0530

    Added ability to change category

--- lib/main.dart
+++ lib/main.dart
@@ -26,15 +26,33 @@ class _HomeState extends State<Home> {
   var _url =
       'https://cdn.shibe.online/cats/7e788e792e7f0181a0d7716e2ffa0b5fc9ecfc0c.jpg';
 
+  var _category = 'cat';
+  var _apiURL = 'http://shibe.online/api/cats';
+
   Future<String> makeRequest() async {
-    var response = await http.get(
-        Uri.encodeFull('http://shibe.online/api/cats'),
-        headers: {"Accept": "application/json"});
+    var response = await http
+        .get(Uri.encodeFull(_apiURL), headers: {"Accept": "application/json"});
     var extractdata = json.decode(response.body);
 
     setState(() {
       _url = extractdata[0];
     });
+  }
+
+  void changeCategory() {
+    if (_category == 'cat') {
+      setState(() {
+        _category = 'dog';
+        _apiURL = 'http://shibe.online/api/shibes';
+      });
+      print('Changed to dog');
+    } else if (_category == 'dog') {
+      setState(() {
+        _category = 'cat';
+        _apiURL = 'http://shibe.online/api/cats';
+      });
+      print('Changed to cat');
+    }
   }
 
   @override
@@ -49,10 +67,27 @@ class _HomeState extends State<Home> {
           alignment: Alignment.center,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: makeRequest,
-        tooltip: 'Increment',
-        child: Icon(Icons.refresh),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: FloatingActionButton(
+              heroTag: null,
+              child: Icon(Icons.repeat),
+              onPressed: changeCategory,
+            ),
+          ),
+          FloatingActionButton(
+            heroTag: null,
+            child: Icon(
+              Icons.refresh,
+            ),
+            backgroundColor: Colors.redAccent,
+            onPressed: makeRequest,
+          ),
+        ],
       ),
     );
   }
