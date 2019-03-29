860a9192b691236b9bebc0c5beffab93af2e6a3b
R3l3ntl3ss <dev.91096@gmail.com>
2019-03-29 20:27:07 +0530

    Converted to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,35 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  var url =
-      'https://cdn.shibe.online/cats/7e788e792e7f0181a0d7716e2ffa0b5fc9ecfc0c.jpg';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Nekinu',
       home: Scaffold(
-        body: Container(
-          child: Image.network(
-            url,
-            fit: BoxFit.cover,
-            height: double.infinity,
-            width: double.infinity,
-            alignment: Alignment.center,
-          ),
-        ),
+        body: Home(),
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var url =
+      'https://cdn.shibe.online/cats/7e788e792e7f0181a0d7716e2ffa0b5fc9ecfc0c.jpg';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.network(
+        url,
+        fit: BoxFit.cover,
+        height: double.infinity,
+        width: double.infinity,
+        alignment: Alignment.center,
       ),
     );
   }
