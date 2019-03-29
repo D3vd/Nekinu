989d7a8f9d92216854cd9b946ce1cdd32b22ee3e
R3l3ntl3ss <dev.91096@gmail.com>
2019-03-29 21:30:04 +0530

    Added Basic Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -8,9 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Nekinu',
-      home: Scaffold(
-        body: Home(),
-      ),
+      home: Home(),
     );
   }
 }
@@ -21,17 +23,36 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  var url =
+  var _url =
       'https://cdn.shibe.online/cats/7e788e792e7f0181a0d7716e2ffa0b5fc9ecfc0c.jpg';
+
+  Future<String> makeRequest() async {
+    var response = await http.get(
+        Uri.encodeFull('http://shibe.online/api/cats'),
+        headers: {"Accept": "application/json"});
+    var extractdata = json.decode(response.body);
+
+    setState(() {
+      _url = extractdata[0];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Image.network(
-        url,
-        fit: BoxFit.cover,
-        height: double.infinity,
-        width: double.infinity,
-        alignment: Alignment.center,
+    return Scaffold(
+      body: Container(
+        child: Image.network(
+          _url,
+          fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+          alignment: Alignment.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: makeRequest,
+        tooltip: 'Increment',
+        child: Icon(Icons.refresh),
       ),
     );
   }
